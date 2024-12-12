ea47e7ab1ab14a49e6005b4709f3fb41c21a3efa
DriesBos <info@driesbos.com>
2024-12-12 19:36:00 +0000

    added Blok Container comp

---
+++ storyblok/BlokContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="blok-Container">
+    <StoryblokComponent
+      v-for="blok in blok.body"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </section>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<style lang="sass">
+.blok-Container
+  border: $test-border
+  display: flex
+  flex-wrap: no-wrap
+  & > div, & > section
+    border: $test-border
+    flex-basis: 50%
+</style>
