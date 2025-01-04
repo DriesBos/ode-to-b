b4434bf33f352174f432135cb4b3b8d789881524
DriesBos <info@driesbos.com>
2025-01-04 13:02:10 +0000

    added empty blok

---
+++ storyblok/EmptyBlok.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="emptyBlok" />
+</template>
+
+<style lang="sass">
+.emptyBlok
+  position: relative
+  width: 100%
+  height: 100%
+  @media screen and (max-width: $breakpoint-mobile)
+    display: none
+</style>
