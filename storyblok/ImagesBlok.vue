44b6e3ae14f04d066ec186911ab3348c877ea30d
DriesBos <info@driesbos.com>
2025-01-04 17:34:42 +0000

    fix: multiple filters

--- storyblok/ImagesBlok.vue
+++ storyblok/ImagesBlok.vue
@@ -1,5 +1,5 @@
 <template>
-  <template v-if="blok.image > 0">
+  <template v-if="blok.image.length > 1">
     <section :id="blok._uid" v-editable="blok" class="imageGrid">
       <ul>
         <li
@@ -109,66 +109,66 @@ defineProps({ blok: Object });
     .imageSingle-ImageContainer
       background-color: transparent !important
 
-// .imageGrid
-//   position: relative
-//   width: 100%
-//   z-index: -1
-//   ul
-//     display: flex
-//     flex-wrap: wrap
-//     width: 100%
-//     padding-left: var(--spacing-two)
-//     padding-right: var(--spacing-two)
-//     li
-//       display: flex
-//       justify-content: center
-//       align-items: center
-//       flex-basis: 50%
-//       @media screen and ( max-width: $breakpoint-mobile)
-//         flex-basis: 100%
-//         transform: translate(0, 0) !important
-//       .imageGrid-Item_Placeholder
-//         position: relative
-//         overflow: visible
-//         background-color: var(--filter-color)
-//         transition: background-color $transition-filter
-//         will-change: background-color, transform
-//         img
-//           width: 100%
-//           height: auto
-//           overflow: visible
-//           opacity: 0.5
-//         &.filter
-//           background-color: var(--filter-color)
-//     @for $i from 1 through 100
-//       li:nth-child(#{$i})
-//         .imageGrid-Item_Placeholder
-//           --y: 0
-//           width: random(40) + 50%
-//           margin-top: random(200) - 100 + px
-//           margin-bottom: random(100) + px
-//           margin-left: random(200) - 100 + px
-//           transform: translateY(calc( #{var(--y) } * 0.3 ))
-//           @media screen and ( max-width: $breakpoint-mobile)
-//             width: 100%
-//             margin-top: 0
-//             margin-bottom: var(--spacing-one)
-//             margin-left: 0
-//     li:first-child, li:nth-child(2)
-//       .imageGrid-Item_Placeholder
-//         @media screen and ( min-width: $breakpoint-mobile)
-//           margin-top: var(--spacing-three)
-//     li:last-child, li:nth-last-child(2)
-//       .imageGrid-Item_Placeholder
-//         @media screen and ( min-width: $breakpoint-mobile)
-//           margin-bottom: 0
-//     li:only-child, li:last-child:nth-child(odd)
-//       flex-basis: 100%
-//       .imageGrid-Item_Placeholder
-//         width: random(20) + 60%
-//         @media screen and ( max-width: $breakpoint-mobile)
-//           width: 100%
-//           margin-top: 0
-//           margin-bottom: var(--spacing-one)
-//           margin-left: 0
+.imageGrid
+  position: relative
+  width: 100%
+  ul
+    display: flex
+    flex-wrap: wrap
+    width: 100%
+    li
+      display: flex
+      justify-content: center
+      align-items: center
+      flex-basis: 50%
+      @media screen and ( max-width: $breakpoint-mobile)
+        flex-basis: 100%
+        transform: translate(0, 0) !important
+      .imageGrid-Item_Placeholder
+        position: relative
+        overflow: visible
+        background-color: var(--filter-color)
+        transition: background-color $transition-filter
+        will-change: background-color, transform
+        // max-height: 100vmin
+        img
+          width: auto
+          max-height: 100vmin
+          object-fit: contain
+          height: auto
+          overflow: visible
+          opacity: 0.5
+        &.filter
+          background-color: var(--filter-color)
+    @for $i from 1 through 100
+      li:nth-child(#{$i})
+        .imageGrid-Item_Placeholder
+          --y: 0
+          width: random(40) + 50%
+          margin-top: random(200) - 100 + px
+          margin-bottom: random(100) + px
+          margin-left: random(200) - 100 + px
+          transform: translateY(calc( #{var(--y) } * 0.3 ))
+          @media screen and ( max-width: $breakpoint-mobile)
+            width: 100%
+            margin-top: 0
+            margin-bottom: var(--spacing-one)
+            margin-left: 0
+    li:first-child, li:nth-child(2)
+      .imageGrid-Item_Placeholder
+        @media screen and ( min-width: $breakpoint-mobile)
+          margin-top: var(--spacing-three)
+    li:last-child, li:nth-last-child(2)
+      .imageGrid-Item_Placeholder
+        @media screen and ( min-width: $breakpoint-mobile)
+          margin-bottom: 0
+    li:only-child, li:last-child:nth-child(odd)
+      flex-basis: 100%
+      .imageGrid-Item_Placeholder
+        width: random(20) + 60%
+        @media screen and ( max-width: $breakpoint-mobile)
+          width: 100%
+          margin-top: 0
+          margin-bottom: var(--spacing-one)
+          margin-left: 0
 </style>
